07ed0b79e691dca70f95a479544961737a8d7ad5
Sonam <87596690+Sonam-05@users.noreply.github.com>
2023-06-07 13:38:10 +0530

    Add files via upload

---
+++ BlogWebappReactRedux/models/postModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+postSchema.index({ title: 'text', description: "text" })
+
+const postModel = mongoose.model("posts", postSchema);
+module.exports = postModel;
